8e5f2c6f11c09845c9ecabb1aad9eede691cd767
Eyosias Mulugeta <iyosiasmulugeta@gmail.com>
2024-08-30 17:00:43 +0300

    Test

---
+++ Uber App/client/app/(tabs)/index.tsx
@@ -0,0 +1,30 @@
+import { View , Text, StatusBar, StyleSheet } from 'react-native';
+
+
+export default function HomeScreen() {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  titleContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 8,
+  },
+  stepContainer: {
+    gap: 8,
+    marginBottom: 8,
+  },
+  reactLogo: {
+    height: 178,
+    width: 290,
+    bottom: 0,
+    left: 0,
+    position: 'absolute',
+  },
+});
---
+++ Uber App/client/tailwind.config.js
@@ -0,0 +1,10 @@
+// tailwind.config.js
+
+module.exports = {
+ content: [],
+  content: ["./App.{js,jsx,ts,tsx}", "./<custom-folder>/**/*.{js,jsx,ts,tsx}"],
+    theme: {
+  extend: { },
+},
+plugins: [],
+}
